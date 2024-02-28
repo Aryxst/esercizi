262d34b9ad88ac828c304ff7da4b278dde35db47
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-02-28 11:50:33 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+	double x, a, b, c, delta, x1, x2;
+	delta = pow(b, 2) - (4 * a * c);
+	if (delta >= 0) {
+		printf("Ci sono due possibili risultati");
+	}
+	return 0;
+}
