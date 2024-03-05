fe4571520587eea79f109c92ae3437734b9fec19
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-03-05 10:38:27 +0100

    Add files via upload

--- es7.c
+++ es7.c
@@ -1,41 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-	int a, b, c, max, min, media, sum;
-	printf("A:");
-	scanf("%d", &a);
-	printf("B:");
-	scanf("%d",&b);
-	printf("C:");
-	scanf("%d", &c);
-	
-	// MAX
-	if(a>b && a>c) {
-		max = a;
-		printf("a e' maggiore di tutti");
-	} 
-	else if (b>c) {
-		max = b;
-		printf("b e' maggiore di tutti");
-	}
-	else if (b<c) {
-		max = c;
-		printf("c e' maggiore di tutti :O");
-	} else {
-		printf("i numberi sono tutti uguali");
-	}
-	// MIN
-	if (max >= b && max >= c) {
-		min = a;
-	} else if (max >= a &&  max >= c) {
-		min  = b;
+	int count, temp, max, min, media, sum;
+	printf("Iterazioni:");
+	scanf("%d", &count);
+
+	int i = 0;
+	for(i; i<count; i++) {
+		printf("Inserisci il numero %d:", i+1);
+		scanf("%d", &temp);
+		if(max < temp) {
+			max = temp;
+		}
+		if (min > temp) {
+			min = temp;
+		}
+		sum += temp;
 		
 	}
-	else if (max >= a && max >= b) {
-		min = c;
-	}
-	sum = a+b+c;
-	media = sum/3;
+	media = sum/count;
 	printf("\nMIN:%d", min);
 	printf("\nMAX:%d", max);
 	printf("\nMEDIA:%d", media);
