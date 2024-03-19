7b3ffeb0a3ba33bdf37f28ea8ec256dda054b167
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-03-19 10:47:18 +0100

    Add files via upload

---
+++ es21.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	char stringa[100]; // Definisci lunghezza max
+	scanf("%s", stringa); // Assegna valore
+	int i = 0;
+	while (stringa[i] != '\0') { // Ripeti finchè il char è valido
+		printf("%d\t", stringa[i]); // Stampa ogni char
+		i++;
+	}
+	return 0;
+}
