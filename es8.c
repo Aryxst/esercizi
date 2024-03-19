e7fabc031510bae7ccffbe6d5935a0532b35ea16
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-03-19 10:53:44 +0100

    Add files via upload

---
+++ es8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int tot,sum,i;
+	printf("Inserisci la quantita' di numeri da sommare:");
+	scanf("%d", &tot);
+
+	for(i = 0; i<tot; i++) {
+		sum += i + 1;
+	}
+	printf("Somma: %d", sum);
+	return 0;
+}
