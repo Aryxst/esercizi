23517931ec65788ef1f136403727784a9820c441
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-03-20 10:30:22 +0100

    Add files via upload

---
+++ es9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int grandezza, k, i;
+    printf("Quanti valori vuoi inserire: ");
+    scanf("%d", &grandezza);
+
+    int numeri[grandezza];
+    for (i = 0; i < grandezza; i++) {
+        printf("Dammi il numero %d: ", i + 1);
+        scanf("%d", &numeri[i]);
+    }
+
+    printf("Quale valore k compreso tra 1 e 4 vuoi utilizzare: ");
+    scanf("%d", &k);
+    while (k < 1 || k > 4) {
+        printf("Il valore di k deve essere compreso tra 1 e 4. Inserisci nuovamente: ");
+        scanf("%d", &k);
+    }
+    printf("Hai scelto il valore k = %d\n\n\n", k);
+    for(i = 0; i< grandezza; i++) {
+    	printf("Il valore numero %d sommato a \"K\" da: %d\n", i, k + numeri[i]);
+	}
+    return 0;
+}
