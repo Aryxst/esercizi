fb848aa7bc1f8215bdde18a8b6a6d571517c8b20
Aryxst <101392520+Aryxst@users.noreply.github.com>
2024-03-20 10:07:21 +0100

    Add files via upload

---
+++ es14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// Inserisco un valore di base "val", che assegno al prossimo valore di fattoriale => "prod"
+	int i, val, prod;
+	printf("Inserisci il valore di base:");
+	scanf("%d", &val);
+	prod = val;
+	// Manda a console ogni risultato del fattoriale, con uno spazio usando "/t"
+	for(i = val - 1 ; i > 1; i--) {
+		prod *=  i;
+		printf("%d\t", prod);
+	}
+	printf("\n\nIl tuo risulato e' %d", prod);
+	return 0;
+}
